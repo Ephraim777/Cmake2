a45f2c79082270eacb51845a0beedbe9fe7dc653
Base <nikitoskey777gmail.com>
2024-06-02 18:40:03 +0300

    initial commit

---
+++ build/CMakeFiles/my_first_cmake_program.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nikita/Desktop/files23/files3/equilateraltriangle.cpp" "CMakeFiles/my_first_cmake_program.dir/equilateraltriangle.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/equilateraltriangle.cpp.o.d"
+  "/home/nikita/Desktop/files23/files3/files3.cpp" "CMakeFiles/my_first_cmake_program.dir/files3.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/files3.cpp.o.d"
+  "/home/nikita/Desktop/files23/files3/isoscelestriangle.cpp" "CMakeFiles/my_first_cmake_program.dir/isoscelestriangle.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/isoscelestriangle.cpp.o.d"
+  "/home/nikita/Desktop/files23/files3/parallelogram.cpp" "CMakeFiles/my_first_cmake_program.dir/parallelogram.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/parallelogram.cpp.o.d"
+  "/home/nikita/Desktop/files23/files3/rect.cpp" "CMakeFiles/my_first_cmake_program.dir/rect.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/rect.cpp.o.d"
+  "/home/nikita/Desktop/files23/files3/rectangle.cpp" "CMakeFiles/my_first_cmake_program.dir/rectangle.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/rectangle.cpp.o.d"
+  "/home/nikita/Desktop/files23/files3/rhombuscpp.cpp" "CMakeFiles/my_first_cmake_program.dir/rhombuscpp.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/rhombuscpp.cpp.o.d"
+  "/home/nikita/Desktop/files23/files3/righttriangle cpp.cpp" "CMakeFiles/my_first_cmake_program.dir/righttriangle_cpp.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/righttriangle_cpp.cpp.o.d"
+  "/home/nikita/Desktop/files23/files3/shape.cpp" "CMakeFiles/my_first_cmake_program.dir/shape.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/shape.cpp.o.d"
+  "/home/nikita/Desktop/files23/files3/trianglecpp.cpp" "CMakeFiles/my_first_cmake_program.dir/trianglecpp.cpp.o" "gcc" "CMakeFiles/my_first_cmake_program.dir/trianglecpp.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/my_first_cmake_program.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_first_cmake_program.dir/equilateraltriangle.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/equilateraltriangle.cpp.o.d"
+  "CMakeFiles/my_first_cmake_program.dir/files3.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/files3.cpp.o.d"
+  "CMakeFiles/my_first_cmake_program.dir/isoscelestriangle.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/isoscelestriangle.cpp.o.d"
+  "CMakeFiles/my_first_cmake_program.dir/parallelogram.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/parallelogram.cpp.o.d"
+  "CMakeFiles/my_first_cmake_program.dir/rect.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/rect.cpp.o.d"
+  "CMakeFiles/my_first_cmake_program.dir/rectangle.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/rectangle.cpp.o.d"
+  "CMakeFiles/my_first_cmake_program.dir/rhombuscpp.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/rhombuscpp.cpp.o.d"
+  "CMakeFiles/my_first_cmake_program.dir/righttriangle_cpp.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/righttriangle_cpp.cpp.o.d"
+  "CMakeFiles/my_first_cmake_program.dir/shape.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/shape.cpp.o.d"
+  "CMakeFiles/my_first_cmake_program.dir/trianglecpp.cpp.o"
+  "CMakeFiles/my_first_cmake_program.dir/trianglecpp.cpp.o.d"
+  "my_first_cmake_program"
+  "my_first_cmake_program.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_first_cmake_program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
